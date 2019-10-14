85e17b5abed805bd87c638d974d258c47b4355cd
Gabriel Viganotti <gabrielviganotti@gmail.com>
2019-10-14 18:59:28 -0300

    feat(authd): implementation of nodejs bindings for SafeAuthdClient API

---
+++ test/auth.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const { new_safe } = require('./helpers.js');
+const { SafeAuthdClient } = require('../lib/index');
+
+describe.only('Authd Client API', () => {
+  let safe_authd_client = new SafeAuthdClient(); // use default port number
+  const secret = `random-secret-${Math.floor(Math.random() * Math.floor(1000))}`;
+  const password = `random-password-${Math.floor(Math.random() * Math.floor(1000))}`;
+  let safe = new_safe();
+  const sk = safe.keys_create_preload_test_coins("10")[1].sk;
+  // TODO: launch authd
+
+  test('Create account', () => {
+    safe_authd_client.create_acc(sk, secret, password);
+  });
+
+  test('Log in', () => {
+    safe_authd_client.log_in(secret, password);
+  });
+
+  test.skip('Log out', () => {
+    safe_authd_client.log_out();
+  });
+
+  test('Subscribe', () => {
+    safe_authd_client.subscribe("https://localhost:33001", (appId) => {
+      console.log("Allow safe-nodejs TEST app?:", appId); return true;
+    });
+  });
+});
