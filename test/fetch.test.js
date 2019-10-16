8f8e1c01a841d6a79a2a829120f697cd134a30a4
Gabriel Viganotti <gabrielviganotti@gmail.com>
2019-10-16 10:32:42 -0300

    feat(inspect): implementation of binding for inspect API

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const { new_safe } = require('./helpers.js');
 
-describe('fetch API', function() {
+describe('fetch/inspect APIs', function() {
   let safe = new_safe();
 
   test('fetch FilesContainer', () => {
@@ -18,6 +18,19 @@ describe('fetch API', function() {
     assert.deepEqual(data.FilesContainer.files_map['/test.txt'], filesMap['/test.txt']);
   });
 
+  test('inspect FilesContainer', () => {
+    let filesContainer = safe.files_container_create("test/testfolder/", "", false, false);
+    let filesContainerXorUrl = filesContainer[0];
+    let filesMap = filesContainer[2];
+
+    let data = safe.inspect(`safe://${filesContainerXorUrl}`);
+    assert.equal(data.FilesContainer.type_tag, 1100);
+    assert.equal(data.FilesContainer.version, 0);
+    assert.equal(data.FilesContainer.data_type, 'PublishedSeqAppendOnlyData');
+    assert.deepEqual(data.FilesContainer.files_map['/test.md'], filesMap['/test.md']);
+    assert.deepEqual(data.FilesContainer.files_map['/test.txt'], filesMap['/test.txt']);
+  });
+
   test('fetch PublishedImmutableData', () => {
     let filesContainer = safe.files_container_create("test/testfolder/test.txt", "", false, false);
     let filesContainerXorUrl = filesContainer[0];
@@ -26,5 +39,16 @@ describe('fetch API', function() {
 
     assert.equal(data.PublishedImmutableData.media_type, 'text/plain');
     assert(String.fromCharCode.apply(null, new Uint8Array(data.PublishedImmutableData.data)).startsWith("hello test.txt!"));
+  });
+
+  test('inspect PublishedImmutableData', () => {
+    let filesContainer = safe.files_container_create("test/testfolder/test.txt", "", false, false);
+    let filesContainerXorUrl = filesContainer[0];
+
+    let fetched = safe.fetch(`safe://${filesContainerXorUrl}/test.txt`);
+
+    let data = safe.inspect(fetched.PublishedImmutableData.xorurl);
+    assert.equal(data.PublishedImmutableData.media_type, 'text/plain');
+    assert.equal(data.PublishedImmutableData.data, "");
   });
 });
