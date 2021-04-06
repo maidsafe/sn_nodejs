42d1138ac2631037e6aa574f71edb69a99638000
Benno Zeeman <bzeeman@live.nl>
2021-04-06 17:01:38 +0200

    feat: CI attempt

--- tests-js/environment-safe.js
+++ tests-js/environment-safe.js
@@ -1,13 +1,29 @@
 const Env = require('jest-environment-node');
 const { Safe } = require('../sn_nodejs');
+const dns = require('dns').promises;
+const net = require('net');
 
 class SafeEnv extends Env {
     async setup() {
         await super.setup();
         this.global.safe = new Safe(undefined, { secs: 120, nanos: 0 });
 
-        const contact = process.env.SN_CONTACT || '127.0.0.1:12000';
-        await this.global.safe.connect(undefined, undefined, [contact]);
+        // Allow a hardcoded bootstrap node to be passed to tests.
+        let contact = process.env.SN_CONTACT;
+        if (contact !== undefined) {
+            // Check if the address provided contains a hostname. If so, resolve it.
+            {
+                const addr = contact.split(':');
+                if (net.isIP(addr[0]) === 0) {
+                    const ip = await dns.lookup(addr[0]); // DNS lookup.
+                    contact = ip.address + ':' + addr[1];
+                }
+            }
+
+            console.log('Connecting to node "%s"', contact);
+            contact = [contact];
+        }
+        await this.global.safe.connect(undefined, undefined, contact);
     }
 
     async teardown() {
